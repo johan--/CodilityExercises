c96c1d965af74c8d3e33aa23af42b7ccd89856db
majerisa <majerisa@gmail.com>
2015-07-23 21:02:51 +0100

    Revert "Improve the performance"
    This reverts commit bd6a65c4d48ca05a31339cbabc4134759e7684b0.

--- FrogJmp.rb
+++ FrogJmp.rb
@@ -1,5 +1,12 @@
-def frogJump(orig,dest,jump_dist)
-  ((dest - orig) / jump_dist.to_f).ceil
+
+def frogJump(origin,dest,jump_dist)
+jumps=0
+current_pos=origin
+while current_pos<dest
+  current_pos+=jump_dist
+  jumps+=1
+end
+return jumps
 end
 
 
